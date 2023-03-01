2ea62bbbac8f93b5eccc72fbf8cafa57e9e3d111
Balakarthickm <123615775+Balakarthickm@users.noreply.github.com>
2023-03-01 20:12:43 +0530

    test

--- src/test/Testing.java
+++ src/test/Testing.java
@@ -1,9 +1,18 @@
 package test;
 
+import java.util.Scanner;
+
 public class Testing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner x= new Scanner(System.in);
+		System.out.println("Enter int value of a:");
+		int a= x.nextInt();
+		System.out.println("Enter int value of b:");
+		int b= x.nextInt();
+		x.close();
+		System.out.println(a+b);
+			
 
 	}
 
